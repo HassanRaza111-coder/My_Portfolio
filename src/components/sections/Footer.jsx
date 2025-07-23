6a63ba378f75be591cfb2853239006d34384e4f4
HassanRaza111-coder <hrmemon786@gmail.com>
2025-07-23 19:00:55 +0500

    Update Footer.jsx

--- src/components/sections/Footer.jsx
+++ src/components/sections/Footer.jsx
@@ -106,7 +106,7 @@ const Footer = () => {
             <Instagram />
           </SocialMediaIcon>
         </SocialMediaIcons>
-        <Copyright>&copy; 2024 Hassan Raza. All rights reserved.</Copyright>
+        <Copyright>&copy; 2025 Hassan Raza. All rights reserved.</Copyright>
       </FooterWrapper>
     </FooterContainer>
   );
